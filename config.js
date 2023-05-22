a83e2ca567d86763db3c1704d24208854b49ab7d
maxwilets <mwilets@gmail.com>
2023-05-22 16:40:27 -0600

    changing URL to graphql url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://sea-lion-app-vvkdr.ondigitalocean.app`;
+export const prodEndpoint = `https://sea-lion-app-vvkdr.ondigitalocean.app/api/graphql`;
 export const perPage = 2;
 export const cors = {
   async rewrites() {
