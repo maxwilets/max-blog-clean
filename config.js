4e52d727a320a261310ed0b0ad0f5c1c5cf39b45
maxwilets <mwilets@gmail.com>
2023-05-22 16:21:42 -0600

    changing config connection

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://api.maxwilets.io/api/graphql`;
+export const prodEndpoint = `https://sea-lion-app-vvkdr.ondigitalocean.app/`;
 export const perPage = 2;
