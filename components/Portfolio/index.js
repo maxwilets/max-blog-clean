4b2bfea5e2c01962e041ac756c9730fed7b55ee3
maxwilets <mwilets@gmail.com>
2023-05-22 18:46:06 -0600

    [FIX] re-adding portfolio component [WORKING]

--- components/Portfolio/index.js
+++ components/Portfolio/index.js
@@ -1,8 +1,8 @@
-import gql from "graphql-tag";
-import { useQuery } from "@apollo/client";
-import { PortfolioStyles } from "./style";
-import DisplayError from "../ErrorMessage";
-import Project from "./Project";
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import { PortfolioStyles } from './style';
+import DisplayError from '../ErrorMessage';
+import Project from './Project';
 
 export const ALL_PORTFOLIO_QUERY = gql`
   query ALL_PORTFOLIO_QUERT {
@@ -30,16 +30,16 @@ export const ALL_PORTFOLIO_QUERY = gql`
 `;
 
 export default function Portfolio() {
-  // const { data, loading, error } = useQuery(ALL_PORTFOLIO_QUERY);
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <DisplayError error={error} />;
-  // const { portfolios: projects } = data;
+  const { data, loading, error } = useQuery(ALL_PORTFOLIO_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+  const { portfolios: projects } = data;
   return (
     <>
       <h1>Projects</h1>
-      {/* {projects.map((project) => (
+      {projects.map((project) => (
         <Project key={project.id} projectData={project} />
-      ))} */}
+      ))}
     </>
   );
 }
