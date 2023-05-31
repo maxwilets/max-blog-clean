d7e68ca602f818f06480c86671a8625bd4e52d52
maxwilets <mwilets@gmail.com>
2023-05-31 16:17:22 -0600

    key and regular to map [UNSTABLE]

--- components/Portfolio/index.js
+++ components/Portfolio/index.js
@@ -4,36 +4,36 @@ import { PortfolioStyles } from './style';
 import DisplayError from '../ErrorMessage';
 import Project from './Project';
 
-const project = [
-  {
-    __typename: 'Project',
-    id: 'proj123',
-    title: 'test project',
-    p1: 'here is a testing project testing UI',
-    p2: 'here is the second paragraph testing the UI for Project',
-    repo: 'https://github.com/maxwilets',
-    liveSite: 'https://maxwilets.io',
-    photo: {
-      altText: 'mountians',
-      image: {
-        publicUrlTransformed:
-          'https://res.cloudinary.com/dsna4d0gs/image/upload/v1640028217/samples/landscapes/nature-mountains.jpg',
-      },
-    },
-    technology: [
-      {
-        id: 'technology123',
-        name: 'javascript',
-        typeLink: 'https://javascript.com',
-      },
-      {
-        id: 'technology1234',
-        name: 'typescript',
-        typeLink: 'https;//typescript.com',
-      },
-    ],
-  },
-];
+// const project = [
+//   {
+//     __typename: 'Project',
+//     id: 'proj123',
+//     title: 'test project',
+//     p1: 'here is a testing project testing UI',
+//     p2: 'here is the second paragraph testing the UI for Project',
+//     repo: 'https://github.com/maxwilets',
+//     liveSite: 'https://maxwilets.io',
+//     photo: {
+//       altText: 'mountians',
+//       image: {
+//         publicUrlTransformed:
+//           'https://res.cloudinary.com/dsna4d0gs/image/upload/v1640028217/samples/landscapes/nature-mountains.jpg',
+//       },
+//     },
+//     technology: [
+//       {
+//         id: 'technology123',
+//         name: 'javascript',
+//         typeLink: 'https://javascript.com',
+//       },
+//       {
+//         id: 'technology1234',
+//         name: 'typescript',
+//         typeLink: 'https;//typescript.com',
+//       },
+//     ],
+//   },
+// ];
 
 export const ALL_PORTFOLIO_QUERY = gql`
   query ALL_PORTFOLIO_QUERT {
@@ -61,28 +61,28 @@ export const ALL_PORTFOLIO_QUERY = gql`
 `;
 
 export default function Portfolio() {
-  // const { data, loading, error } = useQuery(ALL_PORTFOLIO_QUERY);
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <DisplayError error={error} />;
-  // const { portfolios: projects } = data;
+  const { data, loading, error } = useQuery(ALL_PORTFOLIO_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+  const { portfolios: projects } = data;
   return (
-    // <PortfolioStyles>
-    //   <h1>Projects</h1>
-    //   {projects.map((project) => (
-    //     <>
-    //       <Project key={project.id} projectData={project} />
-    //       <hr />
-    //     </>
-    //   ))}
-    // </PortfolioStyles>
     <PortfolioStyles>
       <h1>Projects</h1>
-      {project.map((projects, i) => (
+      {projects.map((project, i) => (
         <>
-          <Project key={projects.id} projectData={projects} />
+          <Project key={project.id} projectData={project} />
           <hr key={i} />
         </>
       ))}
     </PortfolioStyles>
+    // <PortfolioStyles>
+    //   <h1>Projects</h1>
+    //   {project.map((projects, i) => (
+    //     <>
+    //       <Project key={projects.id} projectData={projects} />
+    //       <hr key={i} />
+    //     </>
+    //   ))}
+    // </PortfolioStyles>
   );
 }
