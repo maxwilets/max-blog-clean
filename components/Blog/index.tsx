6a7a8948c25e28f865e8fd43fe32b86b66ce55c4
maxwilets <mwilets@gmail.com>
2024-01-04 15:58:26 -0700

    adding types and convertin blog to TS

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -2,7 +2,20 @@ import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
 import H3Style from '../styles/Typography';
 import { BlogStyle } from './style';
-// import Thumbnail from './Thumbnail';
+// eslint-disable-next-line import/no-unresolved
+import Thumbnail from './Thumbnail';
+
+type Tag = {
+  name: string;
+};
+
+type IProps = {
+  slug: string;
+  title: string;
+  date: string;
+  tags: Tag[];
+  summary: string;
+};
 
 export const ALL_POSTS_QUERY = gql`
   query ALL_POSTS_QUERY {
@@ -30,9 +43,8 @@ export default function Blog() {
       <h1>Blog</h1>
       <H3Style>All Posts</H3Style>
       <section className="cardContainer">
-        {posts.reverse().map((post) => (
-          <p>hello</p>
-          // <Thumbnail post={post} />
+        {posts.reverse().map(({ post }: { post: IProps }) => (
+          <Thumbnail post={post} />
         ))}
       </section>
     </BlogStyle>
