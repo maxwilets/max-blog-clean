2d29ea65820f59d836f90733f37473ed25289382
maxwilets <mwilets@gmail.com>
2024-01-09 15:34:40 -0700

    testing failing proptype

--- components/Education/School.tsx
+++ components/Education/School.tsx
@@ -4,14 +4,14 @@ import { SchoolProp } from '../Types';
 export const School = ({ school }: { school: SchoolProp }): any => {
   <div className="schoolDiv">
     <h4>{school.title}</h4>
-    {/* <p className="large">
-      <img
+    <p className="large">
+      {/* <img
         src={school.photo[0]?.image?.publicUrlTransformed}
         alt={school.photo[0]?.altText}
-      />
+      /> */}
       {school.name}
     </p>
     <p className="large">{school.location}</p>
-    <p className="large">{school.time}</p> */}
+    <p className="large">{school.time}</p>
   </div>;
 };
